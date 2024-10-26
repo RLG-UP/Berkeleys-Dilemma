8af1e7d703033b8bbb211d9007e1206c5f5e3af4
RLG-UP <0262146@up.edu.mx>
2024-10-26 16:15:08 -0600

    Uploading the now working changing color background

--- public/src/listeners.js
+++ public/src/listeners.js
@@ -1,8 +1,13 @@
 const colorRange = document.getElementById('customRange');
-const envBack = document.getElementById('envBod');
-
+const envBack = document.getElementById('idEnvBod');
+//rgb(255, 61, 17)
+//rgb(57, 62, 65)
 colorRange.addEventListener('input', function () {
     const value = this.value; 
+    const R = (57+value)%(256);
+    const G = (62+value)%(62);
+    const B = (65+value)%(18);
     
-    console.log(value); 
+    console.log(R + " " + G + " " + B); 
+    envBack.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
 });
