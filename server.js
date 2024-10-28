e6ca6d146eb2c97888f45e09839059ae0ae96a2e
RLG-UP <0262146@up.edu.mx>
2024-10-28 09:04:43 -0600

    More photos

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (isNaN(slt)) {
     process.exit(1);
 }
 
-const uri = `mongodb+srv://${user}:${pass}@cluster0.ly2jk.mongodb.net/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${user}:${pass}@cluster0.m6rt5.mongodb.net/${db}?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -381,6 +381,140 @@ app.route('/environment')
 
         
         res.render('environment', params);
+    });
+
+
+    app.route('/environment2')
+    .get((req, res)=>{
+        var carrousel = [
+            {
+                src: 'https://www.travelandleisure.com/thmb/qnFoUlxGP3j-1C7uUm04Ut0wqjU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ice-monolith-antarctica-TRVLTOANT0518-cd3a9ff6d76f4e989d9b896a0bb15089.jpg',
+                alt: 'Pristine Antarctica',
+                overlay: 'Untouched landscapes, abundant marine life, and stable ice shelves supporting diverse ecosystems.'
+            },
+            {
+                src: 'https://storage.googleapis.com/travel-web-app-1.appspot.com/flamelink/media/Antartic%20penguins.jpg?GoogleAccessId=firebase-adminsdk-g2s60%40travel-web-app-1.iam.gserviceaccount.com&Expires=16725225600&Signature=GWyZ2Q2dbCdI9jQ5molzj4QSjUKafPDhG8qTazv4A47eTqJhsKbonPjXALgRG%2FT4THnggDGbvqlQO6CWWK8IsHBNirEzxNKmiCr7x3e4WAyYBa1O2HiWQhjflavdeMz%2F%2Blbw0s8D%2Fl7K8U29S5Ux4TkyIPhHIuAL739gdj4u4jyei%2BNDuhhdjk2tO1AbLByOmnTl7TWlHeEZkeEUO7oTuPuN6m9ZgcT4G9hlOXAHQsxEYyMYjtZilBl7heEaJazK%2BL42ukjPcwDn16%2F4L0658bw6E0sW8VgPNLNFxoPAA39QBUjbmRdv7fnhG6%2FRVYLoP5YzKR4U9cI9P%2FtUXneMBg%3D%3D',
+                alt: 'Balanced Ecosystem',
+                overlay: 'Balanced ecosystems with thriving marine and bird species, minimal human impact.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZQYnpopmS1k-yqttBjpmZc6_We2N84DqfhA&s',
+                alt: 'Melting Ice Shelves',
+                overlay: 'Increasing melt rates of ice shelves, impacting habitats for seals, penguins, and other wildlife.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3FxOXhym_AxNXsHKLvB_N4ZlV9qD6h4jOg&s',
+                alt: 'Stressed Ecosystem',
+                overlay: 'Icebergs are melting at accelerated rates. Marine species face habitat reduction and food scarcity.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtUkK6iwe-ejvfpld_DW5wEcv-OgiDMmXutg&s',
+                alt: 'Threatened Biodiversity',
+                overlay: 'Increased ocean temperatures threaten biodiversity and disrupt native species migration patterns.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9eUh4vqlyB2A99oWG_waSYugWtJOWmkqybw&s',
+                alt: 'Fragile Ecosystem',
+                overlay: 'Vulnerable ice shelves and glaciers. Impacts on species dependent on stable, icy habitats.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWDalY4RujZi8yvlYyp_Lul8JzVET1lnE18w&s',
+                alt: 'Declining Penguin Populations',
+                overlay: 'Penguin populations dwindle due to habitat loss, with dwindling food resources and harsh conditions.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb330JVCDwcal7bjDxuGDC_lFKp9tncXkZog&s',
+                alt: 'Severely Impacted Ice Sheets',
+                overlay: 'Rapid ice sheet melting leads to rising sea levels, impacting coastlines globally.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl-s4oaL8C5Oq29r3I09vMgKu6XXielNvmRg&s',
+                alt: 'Endangered Ecosystem',
+                overlay: 'Lack of stable ice severely disrupts the ecosystem, affecting the Antarctic food chain and breeding patterns.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJUR2j4y75T8mAAtyIwENwQru2mW2nkz037A&s',
+                alt: 'Collapsed Ice Ecosystem',
+                overlay: 'Complete ecosystem collapse with minimal wildlife presence. The region has lost most of its former resilience.'
+            }
+        ];
+        
+        
+        const sessUser = req.session.sessUser || null;
+        const params = {
+            carrousel,
+            sessUser,
+        }
+
+        
+        res.render('environment2', params);
+    });
+
+    app.route('/environment3')
+    .get((req, res)=>{
+        var carrousel = [
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTjyZlWwG1Pbsg2Y5Qiek711INFrwa1_wTzQ&s',
+                alt: 'Pristine Tasmania',
+                overlay: 'Untouched landscapes with unique flora and fauna, home to a thriving biodiversity and rare species like the Tasmanian devil.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO-izIhKTsurOsUSu8FuMIRdYUwaS32heKJw&s',
+                alt: 'Balanced Tasmanian Ecosystem',
+                overlay: 'Stable ecosystems with lush forests, clean rivers, and abundant wildlife, minimally impacted by human activity.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUfAvAGdNW7eL_SEiZ_P7dFIiGZarEdbNPYA&s',
+                alt: 'Deforestation Impact',
+                overlay: 'Rising deforestation rates impacting natural habitats. Increased land clearing threatens Tasmania’s biodiversity.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYiq1DBkbTlWOX6_btoLcVKVsKZcwjxKgq1w&s',
+                alt: 'Stressed Ecosystem',
+                overlay: 'Invasive species and habitat destruction put pressure on native wildlife, creating scarcity of resources.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7FmZYZp_m9_vnLMUDDRQ6xVb3wci-9MqnyA&s',
+                alt: 'Threatened Biodiversity',
+                overlay: 'Warming ocean temperatures disrupt marine life along the coasts, affecting fish populations and kelp forests.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyE1dG9ZASw-O2wY0JaSq4gIetFaN4UfaY1w&s',
+                alt: 'Fragile Forest Ecosystem',
+                overlay: 'Logging and mining encroach upon protected forests, altering habitats and threatening endangered species.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO96S0ptPfLcauVyso0PjhxD8K7xycdV8MZw&s',
+                alt: 'Declining Tasmanian Devil Populations',
+                overlay: 'Disease and habitat loss cause a decline in Tasmanian devil populations, endangering this iconic species.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV1X5ojiJKQwVIApdlt1nwUhpegSzC_O9fDQ&s',
+                alt: 'Severely Impacted Coastlines',
+                overlay: 'Coastal erosion and pollution threaten marine ecosystems, affecting both tourism and local fishing industries.'
+            },
+            {
+                src: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Babel_Island_Aerial.jpg',
+                alt: 'Endangered Forests',
+                overlay: 'Logging practices lead to fragmented forests, with many plant and animal species struggling to survive.'
+            },
+            {
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSFkWjtWj18V8z_JO-bD-2fPy753S1VSSE1Q&s',
+                alt: 'Critically Damaged Ecosystem',
+                overlay: 'Ecosystem collapse in key areas, with irreversible loss of biodiversity and long-term environmental impacts.'
+            }
+        ];
+        
+        
+        
+        const sessUser = req.session.sessUser || null;
+        const params = {
+            carrousel,
+            sessUser,
+        }
+
+        
+        res.render('environment3', params);
     });
 
 
