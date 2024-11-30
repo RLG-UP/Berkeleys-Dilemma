2657189c23ddac0c4dc6602b7526f2dbc2d96242
RLG-UP <0262146@up.edu.mx>
2024-11-30 03:41:59 -0600

    Updating

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',  // Change from 'standalone' to 'export'
   images: {
     domains: [
       "i.pinimg.com",
